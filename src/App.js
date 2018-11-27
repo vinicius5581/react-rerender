844a03f68d867106a1f2ca1ce4cc20c20ede24f5
Vinicius Santana <santana@Viniciuss-MacBook-Pro.local>
2018-11-26 18:12:50 -0800

    The problem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Score = ({score}) => {
+  console.log('Render Score');
+  return <p>{score}</p>
+}
+
 class App extends Component {
+  state = {
+    score: 0
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({score: 0});
+    }, 2500);
+  }
+
   render() {
+    console.log('Render App');
     return (
       <div className="App">
-        
+        <Score score={this.state.score} />
       </div>
     );
   }
