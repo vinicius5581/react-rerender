ceb10ef6bfe6a1f08e8b2d7183f2821c7f04be5b
Vinicius Santana <santana@Viniciuss-MacBook-Pro.local>
2018-11-27 00:43:03 -0800

    Updates

---
+++ precache-manifest.f60b8a53d2351bf328af23941e13788e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4cc5560c2ec70c19139",
+    "url": "/react-rerender/static/css/main.3ef14bf0.chunk.css"
+  },
+  {
+    "revision": "f4cc5560c2ec70c19139",
+    "url": "/react-rerender/static/js/main.f4cc5560.chunk.js"
+  },
+  {
+    "revision": "fe65e5159218a8a08e5e",
+    "url": "/react-rerender/static/js/1.fe65e515.chunk.js"
+  },
+  {
+    "revision": "40bc9de168a2c43d5819",
+    "url": "/react-rerender/static/js/runtime~main.40bc9de1.js"
+  },
+  {
+    "revision": "5292009a92c3b81747605f582a05304b",
+    "url": "/react-rerender/index.html"
+  }
+];
